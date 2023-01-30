33f0bb7843f9109438b07e1b8caca58c746716d9
Waleedx7 <w.alshelahi@gmail.com>
2023-01-30 18:14:23 +0300

    first_page

--- event_page/models.py
+++ event_page/models.py
@@ -1,9 +1,15 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+user = get_user_model()
 
 # Create your models here.
 class Event(models.Model):
     name = models.CharField(max_length=100)
     image = models.ImageField(upload_to='events/static')
+    description = models.TextField()
+    location = models.TextField()
+    date_time = models.DateTimeField()
     organiser = models.CharField(max_length=100)
     seats = models.PositiveIntegerField()
     booked_seats = models.PositiveIntegerField(default=0)
